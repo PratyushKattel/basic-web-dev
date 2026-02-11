881fe01afb6604956ab1cfe3f4c05cd843aac8d6
pratyushz0 <pratyushkatel@gmail.com>
2026-02-11 16:15:58 +0545

    dynamic table done

--- dynamic_table/script.js
+++ dynamic_table/script.js
@@ -2,10 +2,14 @@ const inputrow = document.querySelector('#row-num');
 const inputcol = document.querySelector('#col-num');
 const submit_btn = document.querySelector('#submit');
 const table_div = document.querySelector('.table');
+const add_row=document.querySelector('#add-row')
+const add_col=document.querySelector('#add-col')
+const high_even=document.querySelector('#high-even')
+const table = document.createElement('table');
+let row_n,col_n
 
 function initTable(row_n, col_n) {
     table_div.innerHTML = ''; // Clear previous table
-    const table = document.createElement('table');
     table.style.borderCollapse = 'collapse';
 
     for (let i = 0; i < row_n; i++) {
@@ -23,12 +27,39 @@ function initTable(row_n, col_n) {
 
     table_div.appendChild(table);
 }
-
+add_col.addEventListener('click',()=>{
+    col_n+=1
+    for( let row of table.childNodes){
+        td = document.createElement('td');
+            td.style.width = '50px';
+            td.style.height = '20px';
+            td.style.border = '1px solid blue';
+        row.appendChild(td)
+    }
+    
+    })
+high_even.addEventListener('click',()=>{
+    for( let i =0;i<table.childElementCount;i+=2){
+        table.childNodes[i].style.backgroundColor="rgba(255, 255, 0, 0.5)"
+        
+}})
+    // table.appendChild(tr)
+add_row.addEventListener('click',()=>{
+    let tr=document.createElement('tr')
+    for(let i =0;i<col_n;i++){
+        let td = document.createElement('td');
+            td.style.width = '50px';
+            td.style.height = '20px';
+            td.style.border = '1px solid blue';
+            tr.appendChild(td);
+    }
+    table.appendChild(tr)
+})
 submit_btn.addEventListener('click', () => {
-    const rows = parseInt(inputrow.value);
-    const cols = parseInt(inputcol.value);
-    if (!isNaN(rows) && !isNaN(cols)) {
-        initTable(rows, cols);
+     row_n = parseInt(inputrow.value);
+     col_n = parseInt(inputcol.value);
+    if (!isNaN(row_n) && !isNaN(col_n)) {
+        initTable(row_n, col_n);
         console.log("Table generated");
     } else {
         console.log("Invalid input");
